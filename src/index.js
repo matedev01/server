589ffc3987e00939d9fc8f0c507f67d8439a1e85
matedev01 <bmaximovich7@gmail.com>
2025-09-21 04:57:26 +0300

    change

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperties(exports, "_esModule", { value: true});
+const math_1 = require("./test/math");
+const a = 1;
+const b = 2;
+console.log("This is the final result:", (0, math_1.add)(a, b));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {add} from '@/test/math'
+const a = 1
+
+const b =2
+
+//This is going to add a and b and log it to the console
+console.log("This is add",add(a, b))
